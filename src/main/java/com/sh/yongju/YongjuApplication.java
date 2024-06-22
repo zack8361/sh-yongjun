6abe540b9f22de142c7aea62a85bac040805f4e2
zack8361 <zack6353@naver.com>
2024-06-22 17:37:24 +0900

    yongjun firstCommit

---
+++ src/main/java/com/sh/yongju/YongjuApplication.java
@@ -0,0 +1,13 @@
+package com.sh.yongju;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class YongjuApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(YongjuApplication.class, args);
+	}
+
+}
